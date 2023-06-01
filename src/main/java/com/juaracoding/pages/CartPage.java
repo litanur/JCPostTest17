7c851f3f01f2845452b05882a8478a2285f0196b
Lita Nurlaelati <litanurlaelati@gmail.com>
2023-06-01 22:10:23 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/pages/CartPage.java
@@ -0,0 +1,21 @@
+package com.juaracoding.pages;
+
+import com.juaracoding.drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CartPage {
+    private WebDriver driver;
+    public CartPage(){
+        this.driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//a[normalize-space()='View cart']")
+    WebElement cart;
+    public void showCart(){
+        cart.click();
+    }
+}
---
+++ src/main/java/com/juaracoding/pages/ProductPage.java
@@ -0,0 +1,40 @@
+package com.juaracoding.pages;
+
+import com.juaracoding.drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class ProductPage {
+    private WebDriver driver;
+
+    public ProductPage(){
+        this.driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//a[normalize-space()='Black Cross Back Maxi Dress']")
+    WebElement chooseProduct;
+
+    @FindBy(xpath = "//select[@id='pa_color']")
+    WebElement colorOption;
+
+    @FindBy(xpath = "//select[@id='pa_size']")
+    WebElement sizeOption;
+
+    @FindBy(xpath = "//button[normalize-space()='Add to cart']")
+    WebElement addToCart;
+    public void product(String color, String size){
+        driver.get("https://shop.demoqa.com/shop");
+        chooseProduct.click();
+        Select selectColor = new Select(this.colorOption);
+        selectColor.selectByVisibleText(color);
+        selectColor.getFirstSelectedOption().getText();
+        Select selectSize = new Select(this.sizeOption);
+        selectSize.selectByVisibleText(size);
+        selectSize.getFirstSelectedOption().getText();
+        addToCart.click();
+    }
+}
